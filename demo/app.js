4c74b481d1ba664c4ba3ecfa1813c9f57b62f60a
leinue <597055914@qq.com>
2016-10-17 14:22:01 +0800

    update

--- demo/app.js
+++ demo/app.js
@@ -7,14 +7,13 @@ var pty = require('pty.js');
 var terminals = {},
     logs = {};
 
-// app.all('*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("X-Powered-By",' 3.2.1')
-//     res.header("Content-Type", "application/json;charset=utf-8");
-//     next();
-// });
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Accept,X-Requested-With,Sec-Websocket-Version,Sec-Websocket-Extensions,Sec-Websocket-Key,pragma, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Api-Version, Authorization");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use('/build', express.static(__dirname + '/../build'));
 app.use('/addons', express.static(__dirname + '/../addons'));
